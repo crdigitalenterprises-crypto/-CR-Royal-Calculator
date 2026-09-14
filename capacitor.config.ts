f4252fa19c10a73b9f5a4987d8e285dc83af4c6e
Chakar Randhawa <cr.digital.enterprises@gmail.com>
2026-09-14 09:31:02 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cr.royalcalculator',
+  appName: 'CR Royal Calculator',
+  webDir: 'dist'
+};
+
+export default config;
